2f6d69930070906e9cb3f7fd48e08555267a3c06
k00lawn <k00lawn@gmail.com>
2022-08-02 22:47:10 +0530

    file upload works

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
 const app = express()
 
 const port = 3000;
@@ -8,6 +11,11 @@ app.set('view engine', 'ejs')
 app.get('/', (req, res) => {
   res.render('index')
 })
+
+app.post('/upload', upload.single('file'), (req, res) => {
+  res.send('hi')
+})
+
 
 app.listen(port, () => {
   console.log(`file-sharing app listening on port ${port}`)
